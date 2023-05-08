33d9c69068781a94f3d0e8912f9e0a984ff63ec7
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-05-07 21:44:37 -0400

    Initial Commit

---
+++ SwipeDownDemo/SwipeDownDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeDownDemoApp.swift
+//  SwipeDownDemo
+//
+//  Created by Lori Rothermel on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwipeDownDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
