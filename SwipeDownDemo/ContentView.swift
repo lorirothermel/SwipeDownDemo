c31ac5ce2f61646264ba01bb12aa54685902e28c
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-05-07 21:58:58 -0400

    Swipe Down to refresh

--- SwipeDownDemo/ContentView.swift
+++ SwipeDownDemo/ContentView.swift
@@ -8,16 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var items: Int = 1
+    @State private var randomNumbers: [Int] = []
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        NavigationStack {
+            List {
+                ForEach(randomNumbers, id: \.self) { number in
+                    let color = (number == randomNumbers.max() ? Color.green : Color.black)
+                    Text("\(number)")
+                        .foregroundColor(color)
+                }  // ForEach
+            }  // List
+            .navigationTitle("Random Number")
+            .onAppear {
+                getRandomNumbers()
+            }  // .onAppear
+            .refreshable {
+                getRandomNumbers()
+            }
+        }  // NavigationStack
+    }  // some View
+    
+    
+    func getRandomNumbers() {
+        randomNumbers = []
+        
+        for _ in 1...10 {
+            let tempRandomNumber = Int.random(in: 0...100)
+            randomNumbers.append(tempRandomNumber)
+        }  // for
+        
+        randomNumbers.shuffle()
+        
+    }  // getRandomNumbers
+    
+}  // ContentView
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
